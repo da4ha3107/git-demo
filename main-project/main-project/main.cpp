126af5b4e58d34f491415541d593e8cb65da8ff9
da4ha3107 <da4ha3107@yandex.by>
2025-05-24 17:10:22 +0300

    Реализовано форматирование данных при вводе в консоль

--- main-project/main-project/main.cpp
+++ main-project/main-project/main.cpp
@@ -4,31 +4,31 @@
 #include "io.h"
 #include <locale>
 #include <windows.h>
-
+#include "file_reader.h"
 
 int main() {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-
     setlocale(LC_ALL, "Russian");
-    std::vector<Presentation> presentations = readPresentations("data.txt");
+
+    std::vector<Presentation> presentations = readFromFile("data.txt");
 
     std::sort(presentations.begin(), presentations.end(), compareByStartTime);
+    std::cout << "Список докладов:\n-----------------------------\n";
+    for (const auto& p : presentations) {
+        std::cout << p.timeStart << " - " << p.timeEnd << " | "
+            << p.speakerFullName << " | " << p.topic << "\n";
+    }
 
     for (const auto& p : presentations) {
         std::cout << p.timeStart << " - " << p.timeEnd << " | "
-            << p.speakerFullName << " | "
-            << p.topic << "\n";
+            << p.speakerFullName << " | " << p.topic << "\n";
     }
 
     std::string name;
-    std::cout << "Vvtite FIO dliy poiska: ";
-    
-
+    std::cout << "Введите ФИО для поиска: ";
     std::getline(std::cin >> std::ws, name);
     searchBySpeaker(presentations, name);
 
     return 0;
 }
-
-
