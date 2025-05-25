39562fe2f29c96acfab3e72e74398c8cc1f7775f
da4ha3107 <da4ha3107@yandex.by>
2025-05-25 15:02:34 +0300

    Реализована process() для поиска самого длинного доклада

--- main-project/main-project/process.cpp
+++ main-project/main-project/process.cpp
@@ -3,5 +3,16 @@
 #include <sstream>
 
 std::string process(const std::vector<Talk>& talks) {
-    return "00:00"; // заглушка
+    if (talks.empty()) return "00:00"; // Если список докладов пуст, возвращаем "00:00"
+
+    int maxDuration = 0;
+    for (const auto& talk : talks)
+        if (talk.duration_minutes > maxDuration)
+            maxDuration = talk.duration_minutes;
+
+    std::ostringstream result;
+    result << std::setw(2) << std::setfill('0') << maxDuration / 60 << ":"
+        << std::setw(2) << std::setfill('0') << maxDuration % 60;
+
+    return result.str(); // Возвращаем самый длинный доклад в формате ЧЧ:ММ
 }
